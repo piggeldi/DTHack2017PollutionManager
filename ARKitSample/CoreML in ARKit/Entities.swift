831efdd7c22edac1d41e6afc662f058e2a62623d
Frederik Kaiser <frederik.kaiser@telekom.de>
2017-09-19 15:28:04 +0200

    Removed CoreML

---
+++ ARKitSample/CoreML in ARKit/Entities.swift
@@ -0,0 +1,15 @@
+//
+//  Device.swift
+//  CoreML in ARKit
+//
+//  Created by Frederik Ikemann on 19.09.17.
+//  Copyright © 2017 CompanyName. All rights reserved.
+//
+
+import Foundation
+
+struct DeviceCoords: Codable {
+    
+    
+    
+}
