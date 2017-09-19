e634d3115d0a4363b1d0069a842b9c4a5d7a7b6f
Frederik Kaiser <frederik.kaiser@telekom.de>
2017-09-19 15:56:09 +0200

    Storage Models

--- ARKitSample/CoreML in ARKit/Entities.swift
+++ ARKitSample/CoreML in ARKit/Entities.swift
@@ -7,9 +7,36 @@
 //
 
 import Foundation
+import SceneKit
 
-struct DeviceCoords: Codable {
+struct DeviceCoord: Codable {
+    let x: Float
+    let y: Float
+    let z: Float
+    init(vector: SCNVector3) {
+        x = vector.x
+        y = vector.y
+        z = vector.z
+    }
+}
+
+typealias DeviceCoords = [DeviceCoord]
+
+class CoordStorage {
     
+    let userDefaultsKey = "coordinatesStore"
     
+    func fetchAll() -> DeviceCoords? {
+        let jsonEncodedData = UserDefaults.standard.string(forKey: userDefaultsKey)
+        return nil
+    }
     
+    func add(coord: DeviceCoord) {
+        
+        
+    }
+    
+    func remove(coord: DeviceCoord) {
+        
+    }
 }
