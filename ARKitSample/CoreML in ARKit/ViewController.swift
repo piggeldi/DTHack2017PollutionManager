90e6535fcb88bd2053b9faab0f06c554b4f09862
Frederik Kaiser <frederik.kaiser@telekom.de>
2017-09-19 15:30:19 +0200

    Removed CoreML again :D

--- ARKitSample/CoreML in ARKit/ViewController.swift
+++ ARKitSample/CoreML in ARKit/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // COREML
     var visionRequests = [VNRequest]()
     let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
-    @IBOutlet weak var debugTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,21 +48,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Tap Gesture Recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
         view.addGestureRecognizer(tapGesture)
-        
-        //////////////////////////////////////////////////
-        
-        // Set up Vision Model
-        guard let inceptionV3Model = try? VNCoreMLModel(for: Inceptionv3().model) else {
-            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/")
-        }
-        
-        // Set up Vision-CoreML Request
-        let classificationRequest = VNCoreMLRequest(model: inceptionV3Model, completionHandler: classificationCompleteHandler)
-        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
-        visionRequests = [classificationRequest]
-        
-        // Begin Loop to Update CoreML
-        loopCoreMLUpdate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -186,82 +170,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         bubbles.append(bubble)
         return bubbleNodeParent
-    }
-    
-    // MARK: - CoreML Vision Handling
-    
-    func loopCoreMLUpdate() {
-        // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
-        
-        dispatchQueueML.async {
-            // 1. Run Update.
-            self.updateCoreML()
-            
-            // 2. Loop this function.
-            self.loopCoreMLUpdate()
-        }
-        
-    }
-    
-    func classificationCompleteHandler(request: VNRequest, error: Error?) {
-        // Catch Errors
-        if error != nil {
-            print("Error: " + (error?.localizedDescription)!)
-            return
-        }
-        guard let observations = request.results else {
-            print("No results")
-            return
-        }
-        
-        // Get Classifications
-        let classifications = observations[0...1] // top 2 results
-            .flatMap({ $0 as? VNClassificationObservation })
-            .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
-            .joined(separator: "\n")
-        
-        
-        DispatchQueue.main.async {
-            // Print Classifications
-            print(classifications)
-            print("--")
-            
-            // Display Debug Text on screen
-            var debugText:String = ""
-            debugText += classifications
-            self.debugTextView.text = debugText
-            
-            // Store the latest prediction
-            var objectName:String = "…"
-            objectName = classifications.components(separatedBy: "-")[0]
-            objectName = objectName.components(separatedBy: ",")[0]
-            self.latestPrediction = objectName
-            
-        }
-    }
-    
-    func updateCoreML() {
-        ///////////////////////////
-        // Get Camera Image as RGB
-        let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
-        if pixbuff == nil { return }
-        let ciImage = CIImage(cvPixelBuffer: pixbuff!)
-        // Note: Not entirely sure if the ciImage is being interpreted as RGB, but for now it works with the Inception model.
-        // Note2: Also uncertain if the pixelBuffer should be rotated before handing off to Vision (VNImageRequestHandler) - regardless, for now, it still works well with the Inception model.
-        
-        ///////////////////////////
-        // Prepare CoreML/Vision Request
-        let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
-        // let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage!, orientation: myOrientation, options: [:]) // Alternatively; we can convert the above to an RGB CGImage and use that. Also UIInterfaceOrientation can inform orientation values.
-        
-        ///////////////////////////
-        // Run Image Request
-        do {
-            try imageRequestHandler.perform(self.visionRequests)
-        } catch {
-            print(error)
-        }
-        
     }
 }
 
